32234b58498ff7a55d5d09fe572437b0b7827a4a
José Augusto <joseaugustoolliver@gmail.com>
2023-01-21 14:42:02 -0300

    Estrutura de Comigo IMC

---
+++ bin/imc.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  print('Nome: ');
+    String? nome = stdin.readLineSync();
+  int idade = 31;
+  double altura = 1.79;
+  double peso = 89.5;
+  double imc = peso / altura * altura;
+
+  /*if() {}
+  if() {}
+  if() {}
+  if() {}
+  if() {}
+  else() {}*/
+
+}
